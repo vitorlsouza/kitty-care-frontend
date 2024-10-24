4a3bb0719bfeb3d3ccbc7831b1586cec4d99f5db
Rosu <rosumihaialexandru37@gmail.com>
2024-10-24 16:49:19 +0300

    Fix: match and group button actions

--- src/components/Chatroom/SideBar/Icon.tsx
+++ src/components/Chatroom/SideBar/Icon.tsx
@@ -1,6 +1,28 @@
-const Icon = ({ src, className }: { src: string; className: string }) => {
+const Icon = ({
+  src,
+  id,
+  onHover,
+  handleHover,
+}: {
+  src: string;
+  className?: string;
+  id: string;
+  onHover: string;
+  handleHover: (id: string) => void;
+}) => {
   return (
-    <button className={`flex justify-center items-center w-[70px] h-[70px] rounded-lg ${className}`}>
+    <button
+      id={id}
+      className={`flex justify-center items-center w-[70px] h-[70px] rounded-lg p-3.5  ${
+        onHover == id
+          ? "bg-[#FFEEE2]"
+          : id == "lCat"
+          ? "bg-[#FFA500]"
+          : "bg-[#F1D3BB]"
+      }`}
+      onMouseOver={() => handleHover(id)}
+      onMouseLeave={() => handleHover("")}
+    >
       <img src={src} alt="LCat" />
     </button>
   );
--- src/components/Chatroom/SideBar/index.tsx
+++ src/components/Chatroom/SideBar/index.tsx
@@ -1,4 +1,10 @@
 import { useState } from "react";
+
+import Icon from "./Icon";
+import ExtendBtn from "./ExtendBtn";
+import CollapseBtn from "./CollapseBtn";
+import Content from "./Content";
+
 import LCat from "../../../assets/svg/LCat.svg";
 import SCat from "../../../assets/svg/SCat.svg";
 import Health from "../../../assets/svg/Heart.svg";
@@ -7,78 +13,129 @@ import Settings from "../../../assets/svg/Settings.svg";
 import Logout from "../../../assets/svg/Logout.svg";
 import Frame from "../../../assets/svg/Frame.svg";
 import KittyCare from "../../../assets/svg/KittyCare.svg";
-import Icon from "./Icon";
-import ExtendBtn from "./ExtendBtn";
-import CollapseBtn from "./CollapseBtn";
 
 const SideBar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  console.log(isOpen);
+  const [onHover, setOnHover] = useState("");
 
   return (
-    <div className="fixed h-screen flex">
+    <div className="fixed h-screen flex z-20">
       <div className="relative my-12 ml-12 w-auto h-[90%] bg-[#FADFC9] rounded-2xl p-[18px] flex flex-col justify-between">
         <div className="absolute top-[72px] -right-[17%] -translate-x-1 -z-10 hover:cursor-pointer">
           <ExtendBtn handleClick={() => setIsOpen(true)} />
         </div>
         <div className="flex flex-col gap-10">
           <div className="flex">
-            <Icon src={LCat} className="bg-[#FFA500] p-2.5" />
+            <Icon
+              id="lCat"
+              onHover={onHover}
+              src={LCat}
+              handleHover={(id) => setOnHover(id)}
+            />
           </div>
           <div className="flex flex-col gap-4">
-            <Icon src={SCat} className="bg-[#FFEEE2] p-3.5" />
-            <Icon src={Health} className="bg-[#F1D3BB] p-3.5" />
-            <Icon src={Plan} className="bg-[#F1D3BB] p-3.5" />
-            <Icon src={Settings} className="bg-[#F1D3BB] p-3.5" />
+            <Icon
+              id="sCat"
+              onHover={onHover}
+              src={SCat}
+              handleHover={(id) => setOnHover(id)}
+            />
+            <Icon
+              id="health"
+              onHover={onHover}
+              src={Health}
+              handleHover={(id) => setOnHover(id)}
+            />
+            <Icon
+              id="plan"
+              onHover={onHover}
+              src={Plan}
+              handleHover={(id) => setOnHover(id)}
+            />
+            <Icon
+              id="settings"
+              onHover={onHover}
+              src={Settings}
+              handleHover={(id) => setOnHover(id)}
+            />
           </div>
         </div>
         <div className="flex flex-col gap-4">
-          <Icon src={Logout} className="bg-[#F1D3BB] p-3.5" />
-          <Icon src={Frame} className="bg-[#F1D3BB] p-3.5" />
+          <Icon
+            id="logout"
+            onHover={onHover}
+            src={Logout}
+            handleHover={(id) => setOnHover(id)}
+          />
+          <Icon
+            id="frame"
+            onHover={onHover}
+            src={Frame}
+            handleHover={(id) => setOnHover(id)}
+          />
         </div>
       </div>
       <div
-        className={`transition-all duration-300 ${
-          isOpen ? `w-[333px] p-[18px]` : `w-0 p-0`
-        }  my-12 h-[90%] bg-[#F5D7BF] rounded-2xl flex flex-col justify-between overflow-hidden`}
+        className={`transition-all duration-500 my-12 h-[90%] bg-[#F5D7BF] rounded-2xl flex flex-col justify-between overflow-hidden
+          ${isOpen ? `w-[333px] py-[18px]` : `w-0 py-0`}`}
       >
         {isOpen && (
           <>
             <div className="flex flex-col gap-10">
               <div className="flex">
-                <button className="text-[20px] mr-[26px] h-[70px] flex items-center">
+                <button
+                  className={`text-[20px] px-[26px] w-full h-[70px] flex items-center ${
+                    onHover == "lCat" ? "bg-[#FFEEE2] font-bold" : ""
+                  }`}
+                  onMouseOver={() => setOnHover("lCat")}
+                  onMouseLeave={() => setOnHover("")}
+                >
                   <img src={KittyCare} alt="KittyCare" />
                 </button>
               </div>
               <div className="flex flex-col gap-4">
-                <button className="text-[20px] mr-[26px] text-left h-[70px] overflow-hidden leading-10 text-[#625042] hover:font-bold">
-                  Smart-Kitty
-                </button>
-                <button className="text-[20px] mr-[26px] text-left h-[70px] overflow-hidden leading-10 text-[#625042] hover:font-bold">
-                  Health & Wellness
-                </button>
-                <button className="text-[20px] mr-[26px] text-left h-[70px] overflow-hidden leading-10 text-[#625042] hover:font-bold">
-                  Planner
-                </button>
-                <button className="text-[20px] mr-[26px] text-left h-[70px] overflow-hidden leading-10 text-[#625042] hover:font-bold">
-                  Settings
-                </button>
+                <Content
+                  id="sCat"
+                  content="Smart-Kitty"
+                  handleHover={(id) => setOnHover(id)}
+                  onHover={onHover}
+                />
+                <Content
+                  id="health"
+                  content="Health & Wellness"
+                  handleHover={(id) => setOnHover(id)}
+                  onHover={onHover}
+                />
+                <Content
+                  id="plan"
+                  content="Planner"
+                  handleHover={(id) => setOnHover(id)}
+                  onHover={onHover}
+                />
+                <Content
+                  id="settings"
+                  content="Settings"
+                  handleHover={(id) => setOnHover(id)}
+                  onHover={onHover}
+                />
               </div>
             </div>
             <div className="flex flex-col gap-4">
-              <button className="text-[20px] mr-[26px] text-left h-[70px] overflow-hidden leading-10 text-[#625042] hover:font-bold">
-                Logout
-              </button>
-              <button className="text-[20px] mr-[26px] text-left h-[70px] overflow-hidden leading-10 text-[#625042] hover:font-bold">
-                Welcome <b>Rosu</b>
-              </button>
-            </div>
-            <div className="absolute top-24 -right-[52px] -translate-x-1 z-10 hover:cursor-pointer">
-              <CollapseBtn
-                handleClick={() => {
-                  setIsOpen(false);
-                }}
+              <Content
+                id="logout"
+                content="Logout"
+                handleHover={(id) => setOnHover(id)}
+                onHover={onHover}
               />
+              <Content
+                id="frame"
+                content="Welcome Rosu"
+                handleHover={(id) => setOnHover(id)}
+                onHover={onHover}
+              />
+            </div>
+            <div className="absolute top-32 -right-[52px] -translate-x-1 z-10 hover:cursor-pointer">
+              <CollapseBtn handleClick={() => setIsOpen(false)} />
             </div>
           </>
         )}
