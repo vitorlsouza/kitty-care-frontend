2975c3eefe9e3d8adaa7b938206629ce29147c14
Rosu <rosumihaialexandru37@gmail.com>
2024-10-25 13:46:30 +0300

    Fix: add cat assistant redirect link at home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Chatroom from "./pages/Chatroom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/cat-assistant" element={<Chatroom />} />
         </Routes>
       </div>
