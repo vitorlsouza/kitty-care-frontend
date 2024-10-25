ab14dbc90c0f0f171d17be72af8e7aa009fc9f36
Rosu <rosumihaialexandru37@gmail.com>
2024-10-25 18:04:55 +0300

    Fix: add tailwind css to project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@
 const Home = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <a href="/cat-assistant" className="text-2xl">Chatroom</a>
+      <a href="/cat-assistant" className="text-xl">Chatroom</a>
     </div>
   );
 };
