8255ca61b4c8eda44d2413a454c55688634b537a
Rosu <rosumihaialexandru37@gmail.com>
2024-10-26 04:59:01 +0300

    Fix: change forgot password's color

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -33,7 +33,9 @@ const Login = () => {
 
           <div className="w-full h-full flex-col justify-between">
             <div className="my-3 flex flex-col">
-              <label className=" text-base sm:text-xl font-bold sm:font-medium m-4">Email</label>
+              <label className=" text-base sm:text-xl font-bold sm:font-medium m-4">
+                Email
+              </label>
               <div className="w-full h-[52px]">
                 <input
                   className="w-full py-3 sm:py-4 px-4  text-base sm:text-xl border-2 rounded-lg"
@@ -43,7 +45,9 @@ const Login = () => {
               </div>
             </div>
             <div className="my-3 flex flex-col">
-              <label className=" text-base sm:text-xl font-bold sm:font-medium m-4">Password</label>
+              <label className=" text-base sm:text-xl font-bold sm:font-medium m-4">
+                Password
+              </label>
               <div className="w-full h-[52px] relative">
                 <input
                   className="w-full py-3 sm:py-4 px-4  text-base sm:text-xl border-2 rounded-lg pr-12"
@@ -62,8 +66,8 @@ const Login = () => {
                   )}
                 </button>
               </div>
-              <div className="text-[#FF7070] relative mt-5 ml-5 text-[14px] font-medium">
-                Forgot Password?
+              <div className="relative mt-5 ml-5 text-[14px] font-medium">
+                <a href="#">Forgot Password?</a>
               </div>
             </div>
             <div className="my-3">
