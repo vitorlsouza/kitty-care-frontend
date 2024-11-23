788009a6efe06391d34f91fc16934c08859c163e
Muhammad Danyal Jamil Chohan <danyal.jcc@gmail.com>
2024-11-23 19:11:06 +0300

    Max screen sizing on the pre-signup screen

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -135,18 +135,18 @@ export const Signup: React.FC = () => {
   return (
     <Layout>
       {isShowPaywall && isMobile ? (
-        <div className='flex flex-col gap-12 w-full p-[30px_16px]'>
-          <div className='flex flex-col gap-4 text-center'>
-            <h1 className='text-black [leading-trim:both] [text-edge:cap] font-inter text-[28px] font-bold capitalize'>Get KittyCare Today</h1>
-            <p className='text-[#404040] text-center [leading-trim:both] [text-edge:cap] font-inter text-[20px] font-medium leading-[1.4]'>
+        <div className='flex flex-col gap-6 w-full p-[16px] h-auto'>
+          <div className='flex flex-col gap-2 text-center'>
+            <h1 className='text-black [leading-trim:both] [text-edge:cap] font-inter text-[24px] font-bold capitalize'>Get KittyCare Today</h1>
+            <p className='text-[#404040] text-center [leading-trim:both] [text-edge:cap] font-inter text-[18px] font-medium leading-[1.3]'>
               Unlock every exclusive feature - for $0 right now
             </p>
           </div>
-          <div className='flex flex-col gap-[40px] w-full rounded-[22px] border-2 border-[#DBCEC4] bg-[#F3EDE8] p-[0_48px_46px_48px]'>
-            <h3 className='w-full bg-[#FFA500] rounded-b-[14px] px-[28px] py-[12px] text-black text-center [leading-trim:both] [text-edge:cap] font-inter text-sm font-semibold leading-[1.4]'>
+          <div className='flex flex-col gap-[20px] w-full rounded-[22px] border-2 border-[#DBCEC4] bg-[#F3EDE8] p-[0_24px_24px_24px]'>
+            <h3 className='w-full bg-[#FFA500] rounded-b-[14px] px-[20px] py-[8px] text-black text-center [leading-trim:both] [text-edge:cap] font-inter text-sm font-semibold leading-[1.4]'>
               Why KittyCare?
             </h3>
-            <ul className='flex flex-col gap-[22px] text-sm text-black'>
+            <ul className='flex flex-col gap-[16px] text-sm text-black'>
               <li className='flex gap-[10px]'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
                   <path d="M2.84766 9.6533C2.84766 6.07128 2.84766 4.28027 3.96803 3.16748C5.08841 2.05469 6.89162 2.05469 10.498 2.05469C14.1044 2.05469 15.9077 2.05469 17.0281 3.16748C18.1484 4.28027 18.1484 6.07128 18.1484 9.6533C18.1484 13.2353 18.1484 15.0263 17.0281 16.1391C15.9077 17.2519 14.1044 17.2519 10.498 17.2519C6.89162 17.2519 5.08841 17.2519 3.96803 16.1391C2.84766 15.0263 2.84766 13.2353 2.84766 9.6533Z" fill="#FFCE01" stroke="black" stroke-width="1.19973" />
@@ -199,9 +199,9 @@ export const Signup: React.FC = () => {
               </li>
             </ul>
           </div>
-          <div className='flex flex-col gap-[14px]'>
+          <div className='flex flex-col gap-[10px]'>
             <button
-              className="w-full h-[55px] mt-6 text-base sm:text-xl bg-blue-600 text-white rounded-2xl hover:bg-blue-700 active:bg-blue-800 transition-colors duration-200"
+              className="w-full h-[50px] text-base bg-blue-600 text-white rounded-2xl hover:bg-blue-700 active:bg-blue-800 transition-colors duration-200"
               onClick={() => setIsShowPaywall(false)}
             >
               Try For Free
